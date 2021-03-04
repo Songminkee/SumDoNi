9493af30c92e4044535a239cac81596262e54243
Hyunwook Kim <hyunwook.kim.89@gmail.com>
2021-03-04 17:44:58 +0900

    Update SignupForm.

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
 # accounts/views.py
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import SignupForm
+
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignupForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+
+# Reference
+# - Manager isn't available; 'auth.User' has been swapped for 'accounts.User'
+#   https://stackoverflow.com/questions/17873855/manager-isnt-available-user-has-been-swapped-for-pet-person
