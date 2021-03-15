2a0cd25cba200d620b9362783a4a13fac2d66072
Hyunwook Kim <hyunwook.kim.89@gmail.com>
2021-03-15 20:09:07 +0900

    Update TrackingAPI.

--- tracking/views.py
+++ tracking/views.py
@@ -1,26 +1,50 @@
-from django.http import HttpResponse
+# Reference
+# - Messages
+#   https://docs.djangoproject.com/en/3.1/ref/contrib/messages/#django.contrib.messages.storage.fallback.FallbackStorage
+
+
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.contrib import messages
+
+from accounts.models import User
 
 import cv2
 
 
-class TrackingView(TemplateView):
+class TrackingView(LoginRequiredMixin, TemplateView):
+    login_url = '/accounts/login/'
     template_name = 'tracking/tracking.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = User.objects.get(username=self.request.user)
+
+        storage = messages.get_messages(self.request)
+        for message in storage:
+            context['msg'] = message
+            break
+
+        return context
     
 
-class TrackingOnView(View):
+class TrackingOnView(LoginRequiredMixin, View):
+    login_url = '/accounts/login/'
 
     def get(self, request, *args, **kwargs):
-        cctv_id, cctv_pw = 'hanamart-public', 'hanamart-public'
-        ip = '121.151.100.57'
-        port = 9001
+        user = User.objects.get(username=request.user)
+        cctv_id, cctv_pw = user.cctv_id, user.cctv_pw
+        cctv_ip = user.cctv_ip
+        cctv_port = user.cctv_port
         cctv_quality = 'sub' # main: QHD, sub: HD or SD?
-        url = f'rtsp://{cctv_id}:{cctv_pw}@{ip}:{port}//h264Preview_01_{cctv_quality}'
+        url = f'rtsp://{cctv_id}:{cctv_pw}@{cctv_ip}:{cctv_port}//h264Preview_01_{cctv_quality}'
 
         # Check IP camera activation
         repeat_count, is_activate = 0, False
-        while repeat_count < 10:
+        while repeat_count < 3:
             cap = cv2.VideoCapture(url)
             if not cap.isOpened():
                 cv2.waitKey(1)
@@ -31,18 +55,23 @@ class TrackingOnView(View):
                 break
 
         if is_activate:
-            return HttpResponse('IP camera is activated.')
+            user = User.objects.get(username=request.user)
+            user.tracking_status = True
+            user.save()
+            return HttpResponseRedirect('/tracking')
         else:
-            return HttpResponse('IP camera is not activated.')
+            messages.add_message(request, messages.INFO, 'IP Camera is not activated.')
+            return HttpResponseRedirect('/tracking')
 
     def post(self, request, *args, **kwargs):
         return HttpResponse('Tracking On.')
 
 
-class TrackingOffView(View):
+class TrackingOffView(LoginRequiredMixin, View):
+    login_url = '/accounts/login/'
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('Tracking Off.')
-
-    def post(self, request, *args, **kwargs):
-        return HttpResponse('Tracking Off.')
+        user = User.objects.get(username=self.request.user)
+        user.tracking_status = False
+        user.save()
+        return HttpResponseRedirect('/tracking')
