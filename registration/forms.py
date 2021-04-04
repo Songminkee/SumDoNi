9cbcb729774e8f05b8b0b3dd4b2d9247d2488a8b
sung0471 <zzererer@naver.com>
2021-04-04 22:22:36 +0900

    210404 - 1 commit registration/forms.py
    - define form to upload multiple images

--- registration/forms.py
+++ registration/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class UploadImageForm(forms.Form):
     img = forms.ImageField()
+
+
+class UploadMultiImageForm(forms.Form):
+    img = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
